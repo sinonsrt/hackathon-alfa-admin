6cf82cc74b31cc645122f64dfb2ecb318fb07c55
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-12 16:46:31 -0300

    feat: adiciona config S3

--- controllers/vehiclesController.js
+++ controllers/vehiclesController.js
@@ -1,8 +1,15 @@
-const { Vehicles } = require('../models')
+const { Vehicles } = require('../models');
+
+const { uploadFile } = require('../config/s3');
 
 module.exports = {
 
     save: async (req, res) => {
+
+        console.log(req.file);
+
+        const awsUpload = await uploadFile(req.file);
+        console.log(awsUpload);
         
         let data = {
             modelo: req.body.modelo,
@@ -10,7 +17,7 @@ module.exports = {
             anofabricacao: req.body.anofabricacao,
             valor: req.body.valor,
             tipo: req.body.tipo,
-            foto: req.file.filename,
+            foto: awsUpload.Location,
             destaque: req.body.destaque,
             opcionais: req.body.opcionais,
             marca_id: req.body.marca_id,
