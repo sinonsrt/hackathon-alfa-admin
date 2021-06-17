2035774ffffc6cb56778c2e799f73aa06aab5849
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-17 00:55:43 -0300

    feat: contadores dash

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+	Vehicles,
+	Colors,
+	Brands,
+	sequelize
+} = require('../models');
+const { QueryTypes } = require('sequelize');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+
+	const [novos] = await sequelize.query(
+		`SELECT COUNT(*) AS novos
+				FROM Vehicles v 
+				WHERE v.tipo = 'Novo'
+		`, { type: QueryTypes.SELECT }
+	)
+
+	const [seminovos] = await sequelize.query(
+		`SELECT COUNT(*) AS seminovos
+				FROM Vehicles v 
+				WHERE v.tipo = 'Semi-novo'
+		`, { type: QueryTypes.SELECT }
+	)
+
+	const [destaques] = await sequelize.query(
+		`SELECT COUNT(*) AS destaques
+				FROM Vehicles v 
+				WHERE v.destaque = 1
+		`, { type: QueryTypes.SELECT }
+	)
+
+	const data = {
+		novos,
+		seminovos,
+		destaques
+	}
+
+	console.log(data)
+
+	res.render('index', {data});
+
 });
 
 module.exports = router;
